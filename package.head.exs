8fda6daa99f176872d0be1cba282e40176bbed9c
ILYA Khlopotov <ilya.khlopotov@gmail.com>
2012-09-29 21:55:08 -0700

    Adding expm package file.

---
+++ package.head.exs
@@ -0,0 +1,7 @@
+Expm.Package.new(
+  name: "exlager",
+  description: "Elixir wrapper over basho/lager",
+  version: :head,
+  keywords: ["Elixir","Erlang","logging","lager"],
+  maintainers: [[name: "ILYA Khlopotov", email: "ilya.khlopotov@gmail.com"]],
+  repositories: [[github: "khia/exlager"]])
