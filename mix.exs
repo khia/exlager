f96bd92183b0f9e6a629a3062f8f334f4ad7c3c5
jsvisa <delweng@gmail.com>
2014-07-10 17:05:30 +0800

    Lock Elixir's version equal or greater than 0.14

--- mix.exs
+++ mix.exs
@@ -2,13 +2,16 @@ defmodule Exlager.Mixfile do
   use Mix.Project
 
   def project do
-   [
-    app: :exlager, version: "0.2.1", deps: deps
-   ]
+    [
+      app: :exlager,
+      version: "0.14.0",
+      elixir: "> 0.14.0",
+      deps: deps
+    ]
   end
 
   def application do
-     [
+    [
       applications: [
         :compiler,
         :syntax_tools,
@@ -19,7 +22,7 @@ defmodule Exlager.Mixfile do
 
   defp deps do
     [
-     {:lager, git: "https://github.com/basho/lager.git"},
+      {:lager, git: "https://github.com/basho/lager.git"},
     ]
   end
 end
